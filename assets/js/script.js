4bcd4696ed6b6e981470831dea796a47066cc22d
ceshort23 <ceshort23@gmail.com>
2021-04-29 18:26:18 -0500

    allow page to repeat without refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,12 @@ var greetingEl = document.querySelector("#hello")
 var buttonEl = document.querySelector("#btn")
 
 var sayHello = function() {
-    greetingEl.textContent = "Hello World!" 
+    if (greetingEl.textContent === "Hello World!") {
+        greetingEl.textContent = ""
+    }
+    else {
+        greetingEl.textContent = "Hello World!"
+    }
 }
 
 buttonEl.addEventListener("click", sayHello)
